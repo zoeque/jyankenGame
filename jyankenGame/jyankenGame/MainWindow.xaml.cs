773abc85e12567f8bdb4c3134316d494b7db0061
zoeque <121230485+zoeque@users.noreply.github.com>
2024-01-27 22:04:38 +0900

    initial commit

---
+++ jyankenGame/jyankenGame/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using jyankenGame.usecase;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jyankenGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ResultDecideService resultDecideService;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.resultDecideService = new ResultDecideService();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultDecideService.ServiceWinGame())
+            {
+                ResultLabel.Content = "パー";
+            }
+            else
+            {
+                ResultLabel.Content = "チョキ";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (resultDecideService.ServiceWinGame())
+            {
+                ResultLabel.Content = "グー";
+            }
+            else
+            {
+                ResultLabel.Content = "パー";
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (resultDecideService.ServiceWinGame())
+            {
+                ResultLabel.Content = "チョキ";
+            }
+            else
+            {
+                ResultLabel.Content = "グー";
+            }
+        }
+    }
+}
---
+++ jyankenGame/jyankenGame/usecase/ResultDecideService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jyankenGame.usecase
+{
+    class ResultDecideService
+    {
+        private static int RATE = 100;
+        public Boolean ServiceWinGame()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, RATE);
+            if (num == 1) return false;
+            return true;
+        }
+    }
+}
